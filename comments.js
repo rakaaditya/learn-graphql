33ab4dde0b886b92f63aee8e0dd2bf4a50ea4618
raka aditya <adm@rakaaditya.com>
2016-06-04 19:25:45 +0700

    Initial commit

---
+++ comments.js
@@ -0,0 +1,29 @@
+const commentData = [
+  {
+    "id": 1,
+    "content": "Ini contoh komentar 1",
+    "authorName": "Komentator 1"
+  },
+  {
+    "id": 2,
+    "content": "Ini contoh komentar 2",
+    "authorName": "Komentator 2"
+  },
+  {
+    "id": 3,
+    "content": "Ini contoh komentar 3",
+    "authorName": "Komentator 3"
+  },
+  {
+    "id": 4,
+    "content": "Ini contoh komentar 4",
+    "authorName": "Komentator 4"
+  },
+  {
+    "id": 5,
+    "content": "Ini contoh komentar 5",
+    "authorName": "Komentator 5"
+  }
+]
+
+export default commentData
---
+++ index.js
@@ -0,0 +1,76 @@
+var graphql = require('graphql')
+var graphqlHTTP = require('express-graphql')
+var express = require('express')
+
+import * as _ from 'underscore';
+
+import {
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLEnumType,
+  GraphQLNonNull,
+  GraphQLInterfaceType
+} from 'graphql'
+
+import postData from './posts.js'
+import commentData from './comments.js'
+
+// const postData = require('./posts.json')
+
+const Comment = new graphql.GraphQLObjectType({
+  name: 'Comment',
+  fields: () => ({
+    id: { type: GraphQLInt },
+    authorName: { type: GraphQLString },
+    content: { type: GraphQLString }
+  })
+})
+
+const Post = new graphql.GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    id: { type: GraphQLInt },
+    title: { type: GraphQLString },
+    content: {  type: GraphQLString },
+    authorName: { type: GraphQLString },
+    comment: {
+      type: new GraphQLList(Comment),
+      args: {
+        limit: {type: GraphQLInt }
+      },
+      resolve: function(Post, {limit}) {
+        if(limit >= 0) {
+          return commentData.slice(0, limit);
+        }
+        return commentData;
+      }
+    },
+  })
+})
+
+var schema = new graphql.GraphQLSchema({
+  query: new graphql.GraphQLObjectType({
+    name: 'getPost',
+    fields: {
+      post: {
+        type: Post,
+        args: {
+          id: { type: GraphQLInt }
+        },
+        resolve: (source, {id}) => {
+          return _.filter(postData, post => post.id === id)[0];
+        }
+      }
+    }
+  })
+})
+
+express()
+  .use('/posts', graphqlHTTP({ schema: schema, pretty: true }))
+  .listen(3000)
+
+console.log('Runing...')
