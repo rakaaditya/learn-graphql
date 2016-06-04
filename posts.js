adf08c3330852160fc2a1cab20ca90f7cd1a348b
raka aditya <adm@rakaaditya.com>
2016-06-04 19:27:48 +0700

    Remove comments

--- posts.js
+++ posts.js
@@ -1,30 +1,3 @@
-// {
-//   "1": {
-//     "id": "1",
-//     "title": "BI: 80% Pertumbuhan Ekonomi Asia Akan Bertumpu Pada Perkotaan",
-//     "content": "Beberapa tahun mendatang, perkotaan akan menjadi pusat pertumbuhan ekonomi Asia hingga 80%. Perkembangan perkotaan sebagai pusat perekonomian akan terjadi 2035.",
-//     "authorName": "Admin"
-//   },
-//   "2": {
-//     "id": "2",
-//     "title": "Ditinggal Pengasuh Ngaji, Bocah 5 Tahun di Ciracas Diduga Dicabuli Tetangga",
-//     "content": "Bocah berumur 5 Tahun di Ciracas, Jakarta Timur, diduga menjadi korban pencabulan tetangganya.",
-//     "authorName": "Admin"
-//   },
-//   "3": {
-//     "id": "3",
-//     "title": "Gorila AS Ditembak Mati Ingatkan Insiden 1996 Saat Gorila Lindungi Bocah",
-//     "content": "Insiden bocah jatuh ke kandang gorila di AS mengingatkan pada insiden serupa 20 tahun lalu. Saat itu seekor gorila melindungi balita yang jatuh ke kandangnya.",
-//     "authorName": "Admin"
-//   },
-//   "4": {
-//     "id": "4",
-//     "title": "Sopir 5 Trayek di Bandung yang Demo Minta Bus Sekolah Dikandangkan",
-//     "content": "Hari ini bus sekolah di Bandung dikandangkan karena adanya demo dari ratusan sopir angkot. Para sopir minta penghentian operasi bus itu tidak hanya hari ini.",
-//     "authorName": "Admin"
-//   }
-// }
-
 const postData = [
   {
     "id": 1,
